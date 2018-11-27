a5109ca0c19249f2aba87eb23202f0fd5d7edfbe
Rockford Lhotka <rocky@lhotka.net>
2018-11-26 18:23:52 -0600

    Add initial services demo code

---
+++ src/02-services/Gateway/Pages/Index.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gateway.Pages
+{
+  public class IndexModel : PageModel
+  {
+    private static IConnection connection;
+
+    [BindProperty]
+    public string UserInput { get; set; }
+
+    [BindProperty]
+    public string ReplyText { get; set; }
+
+    public void OnGet()
+    {
+      if (connection == null)
+      {
+        var factory = new ConnectionFactory() { HostName = "40.117.117.72" };
+        connection = factory.CreateConnection();
+      }
+    }
+
+    public async Task OnPost()
+    {
+      SendMessage(UserInput);
+      await WaitForReply();
+    }
+
+    private async Task WaitForReply()
+    {
+      using (var channel = connection.CreateModel())
+      {
+        channel.QueueDeclare(queue: "gateway", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+          var message = Encoding.UTF8.GetString(ea.Body);
+          ReplyText = message;
+        };
+        channel.BasicConsume(queue: "gateway", autoAck: true, consumer: consumer);
+        await Task.Delay(100);
+      }
+    }
+
+    private void SendMessage(string message)
+    {
+      using (var channel = connection.CreateModel())
+      {
+        channel.QueueDeclare(queue: "greeter", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        var props = channel.CreateBasicProperties();
+        props.ReplyTo = "gateway";
+        props.CorrelationId = Guid.NewGuid().ToString();
+        channel.BasicPublish(exchange: "", routingKey: "greeter", basicProperties: props, body: body);
+      }
+    }
+  }
+}
