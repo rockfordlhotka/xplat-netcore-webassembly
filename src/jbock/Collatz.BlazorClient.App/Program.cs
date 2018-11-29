76b2449410966b845db426699d8283cfbaa718de
Jason Bock <jason@jasonbock.net>
2018-11-28 20:07:10 -0600

    Added Blazor example

--- src/jbock/Collatz.BlazorClient.App/Program.cs
+++ src/jbock/Collatz.BlazorClient.App/Program.cs
@@ -4,10 +4,8 @@ namespace Collatz.BlazorClient.App
 {
 	public class Program
 	{
-		public static void Main(string[] args)
-		{
+		public static void Main(string[] args) => 
 			CreateHostBuilder(args).Build().Run();
-		}
 
 		public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
 			 BlazorWebAssemblyHost.CreateDefaultBuilder()
--- src/jbock/Collatz.BlazorClient.App/Startup.cs
+++ src/jbock/Collatz.BlazorClient.App/Startup.cs
@@ -6,16 +6,12 @@ namespace Collatz.BlazorClient.App
 {
 	public class Startup
 	{
-		public void ConfigureServices(IServiceCollection services)
-		{
+		public void ConfigureServices(IServiceCollection services) =>
 			// Since Blazor is running on the server, we can use an application service
 			// to read the forecast data.
 			services.AddSingleton<WeatherForecastService>();
-		}
 
-		public void Configure(IBlazorApplicationBuilder app)
-		{
+		public void Configure(IBlazorApplicationBuilder app) => 
 			app.AddComponent<App>("app");
-		}
 	}
 }
