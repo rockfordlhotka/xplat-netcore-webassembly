24d027c47b2f1f902c3c259d6fef97a376de17ce
Jason Bock <jason@jasonbock.net>
2018-11-29 21:37:52 -0600

    Trying to get a Ooui.Wasm example working....not having a lot of success.

---
+++ src/jbock/Collatz.OouiClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using Collatz.Core;
+using Ooui;
+using System.Numerics;
+
+namespace Collatz.OouiClient
+{
+	class Program
+	{
+		static void Main()
+		{
+			var title = new Heading("Collatz.Ooui");
+			var input = new Input(InputType.Number);
+			var button = new Button("Calculate");
+			var result = new Div();
+
+			button.Click += (s, e) => 
+			{
+				if (BigInteger.TryParse(input.Value, out var start))
+				{
+					var sequence = new CollatzSequence(start);
+					result.Text = string.Join(", ", sequence.Sequence);
+				}
+				else
+				{
+					result.Text = $"{input.Value} is not a valid BigInteger.";
+				}
+			};
+
+			var container = new Div(title, input, button, result);
+
+			UI.Publish("/", container);
+		}
+	}
+}
