886824c6d0b455afff14a4f7972788293a4890e0
Rockford Lhotka <rocky@lhotka.net>
2018-11-29 23:13:10 -0600

    Working version of the code

--- src/02-services/RabbitQueue/Queue.cs
+++ src/02-services/RabbitQueue/Queue.cs
@@ -9,9 +9,10 @@ namespace RabbitQueue
   /// <summary>
   /// Helper methods to simplify use of RabbitMQ
   /// </summary>
-  public class Queue
+  public class Queue : IDisposable
   {
     private IConnection connection;
+    private IModel channel;
     private readonly string serviceName;
 
     public Queue(string hostName, string serviceName)
@@ -19,6 +20,7 @@ namespace RabbitQueue
       this.serviceName = serviceName;
       var factory = new ConnectionFactory() { HostName = hostName };
       connection = factory.CreateConnection();
+      channel = connection.CreateModel();
     }
 
     public void SendMessage(string destination, string correlationId, object request)
@@ -34,35 +36,35 @@ namespace RabbitQueue
     private void SendMessage(string sender, string destination, string correlationId, object request)
     {
       var message = JsonConvert.SerializeObject(request);
-      using (var channel = connection.CreateModel())
-      {
-        channel.QueueDeclare(queue: destination, durable: false, exclusive: false, autoDelete: false, arguments: null);
+      channel.QueueDeclare(queue: destination, durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-        var body = Encoding.UTF8.GetBytes(message);
+      var body = Encoding.UTF8.GetBytes(message);
 
-        var props = channel.CreateBasicProperties();
-        if (!string.IsNullOrWhiteSpace(sender))
-          props.ReplyTo = sender;
-        props.CorrelationId = correlationId;
-        props.Type = request.GetType().Name;
-        channel.BasicPublish(exchange: "", routingKey: destination, basicProperties: props, body: body);
-      }
+      var props = channel.CreateBasicProperties();
+      if (!string.IsNullOrWhiteSpace(sender))
+        props.ReplyTo = sender;
+      props.CorrelationId = correlationId;
+      props.Type = request.GetType().Name;
+      channel.BasicPublish(exchange: "", routingKey: destination, basicProperties: props, body: body);
     }
 
     public void StartListening(Action<BasicDeliverEventArgs, string> handleMessage)
     {
-      using (var channel = connection.CreateModel())
-      {
-        channel.QueueDeclare(queue: serviceName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+      channel.QueueDeclare(queue: serviceName, durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-        var consumer = new EventingBasicConsumer(channel);
-        consumer.Received += (model, ea) => 
-        {
-          var message = Encoding.UTF8.GetString(ea.Body);
-          handleMessage(ea, message);
-        };
-        channel.BasicConsume(queue: serviceName, autoAck: true, consumer: consumer);
-      }
+      var consumer = new EventingBasicConsumer(channel);
+      consumer.Received += (model, ea) =>
+      {
+        var message = Encoding.UTF8.GetString(ea.Body);
+        handleMessage(ea, message);
+      };
+      channel.BasicConsume(queue: serviceName, autoAck: true, consumer: consumer);
+    }
+
+    public void Dispose()
+    {
+      channel.Close();
+      connection.Close();
     }
   }
 }
