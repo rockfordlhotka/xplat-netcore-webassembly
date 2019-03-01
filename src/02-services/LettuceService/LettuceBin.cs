e9402325f20d26ab70cba6cc271a12b99980ae17
Rockford Lhotka <rocky@lhotka.net>
2019-02-28 16:42:12 -0800

    Switch to load queue info from env

--- src/02-services/LettuceService/LettuceBin.cs
+++ src/02-services/LettuceService/LettuceBin.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using RabbitMQ.Client.Events;
 using RabbitQueue;
 using System;
@@ -9,10 +10,17 @@ namespace LettuceService
 {
   class LettuceBin
   {
-    private static Queue _queue = new Queue("dinky-wallaby-rabbitmq.default.svc.cluster.local", "lettucebin");
+    private static Queue _queue;
 
     static async Task Main(string[] args)
     {
+      var config = new ConfigurationBuilder()
+        .AddEnvironmentVariables()
+        .Build();
+
+      if (_queue == null)
+        _queue = new Queue(config["rabbitmq:url"], "lettucebin");
+
       Console.WriteLine("### Lettuce bin service starting to listen");
       _queue.StartListening(HandleMessage);
 
